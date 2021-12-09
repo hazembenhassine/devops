b9bef6c784f89766dd8b8edb69b5ebdfae4f4e1b
Hazem Ben Hassine <hazem.benhassine@vynd.com>
2021-12-09 02:30:17 +0100

    Set up tests

--- src/services/building-blocks.service.spec.ts
+++ src/services/building-blocks.service.spec.ts
@@ -1,33 +1,45 @@
 import { BuildingBlocksService } from './building-blocks.service'
 
 
-describe('BuildingBlocks', () => {
+describe('BuildingBlocksService', () => {
     let buildingBlocks: BuildingBlocksService;
     let values: number[];
-    describe('getCollectedWater', () => {
-        it('simple test 1: should return the value of collected water inside the buildings', () => {
+    describe('getCollectedWaterOptimized', () => {
+        test('simple case 1', () => {
+            values = [0, 1, 0, 2];
+            const expectedValue = 1;
+            buildingBlocks = new BuildingBlocksService(values);
+            expect(buildingBlocks.getCollectedWater()).toBe(expectedValue);
+        });
+        test('simple case 2', () => {
+            values = [0, 0, 0, 0];
+            const expectedValue = 0;
+            buildingBlocks = new BuildingBlocksService(values);
+            expect(buildingBlocks.getCollectedWater()).toBe(expectedValue);
+        });
+        test('simple case 3', () => {
+            values = [0, 1, 2, 0];
+            const expectedValue = 0;
+            buildingBlocks = new BuildingBlocksService(values);
+            expect(buildingBlocks.getCollectedWater()).toBe(expectedValue);
+        });
+        test('complex case 1', () => {
             values = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
             const expectedValue = 6;
             buildingBlocks = new BuildingBlocksService(values);
             expect(buildingBlocks.getCollectedWater()).toBe(expectedValue);
         });
-        it('simple test 2: should return 0 when building heights are even', () => {
-            values = [5, 5, 5, 5, 5, 5];
+        test('complex case 2', () => {
+            values = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
             const expectedValue = 0;
             buildingBlocks = new BuildingBlocksService(values);
             expect(buildingBlocks.getCollectedWater()).toBe(expectedValue);
         });
-        it('limit test 1: should return correct value if first building is highest building', () => {
-            values = [9, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
-            const expectedValue = 14;
+        test('edge case', () => {
+            values = [];
+            const expectedValue = 0;
             buildingBlocks = new BuildingBlocksService(values);
             expect(buildingBlocks.getCollectedWater()).toBe(expectedValue);
         });
     });
-});
-
-describe('is working', () => {
-    it('should work', () => {
-        expect(true).toBeTruthy();
-    })
 });
